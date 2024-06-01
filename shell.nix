1a3266831ec2500a3e1be4c2155e19a54b3652b8
Joao Thallis <joaothallis@icloud.com>
2024-06-01 16:59:45 -0300

    Add erlkaf

--- shell.nix
+++ shell.nix
@@ -1,4 +1,13 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
-pkgs.mkShell { nativeBuildInputs = with pkgs.buildPackages; [ elixir_1_16 ]; }
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [
+    elixir_1_16
+    cyrus_sasl
+    lz4
+    rdkafka
+    zlib
+    curl
+  ];
+}
