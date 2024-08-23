94ec093e496b6ad81ec198700c6de863680b1cce
João Thallis <joaothallis@icloud.com>
2024-08-22 23:04:26 -0300

    Rescue when exception

--- random_number/lib/random_number/producer.ex
+++ random_number/lib/random_number/producer.ex
@@ -3,16 +3,30 @@ defmodule RandomNumber.Producer do
 
   @behaviour :erlkaf_producer_callbacks
 
+  require Logger
+
   def create_producer do
     :erlkaf.start()
 
-    producer_config = [ bootstrap_servers: "localhost:9092" ]
+    producer_config = [bootstrap_servers: "localhost:9092"]
 
+    # FIXME: it is not returning error if the server is down
+    # it is only logging the errors
     :ok = :erlkaf.create_producer(:client_producer, producer_config)
-    :ok = :erlkaf.create_topic(:client_producer, @topic, [request_required_acks: -1])
+    :ok = :erlkaf.create_topic(:client_producer, @topic, request_required_acks: -1)
+  rescue
+    exception ->
+      Logger.error("Error to create producer", exception: inspect(exception))
+
+      reraise(exception, __STACKTRACE__)
   end
 
   def produce(key, value) do
-    :ok = :erlkaf.produce(:client_producer, @topic, key, value)
+    :erlkaf.produce(:client_producer, @topic, key, value)
+  rescue
+    exception ->
+      Logger.error("Error to produce message", exception: inspect(exception))
+
+      reraise(exception, __STACKTRACE__)
   end
 end
--- random_number/test/random_number/producer_test.exs
+++ random_number/test/random_number/producer_test.exs
@@ -3,6 +3,11 @@ defmodule RandomNumber.ProducerTest do
 
   test "produce" do
     assert :ok = RandomNumber.Producer.create_producer()
-    assert :ok = RandomNumber.Producer.produce("key", "value")
+
+    :timer.sleep(250)
+
+    # FIXME: request_required_acks: -1 is not working and if I drop the server it's returning :ok
+    # There is no log
+    assert :ok = RandomNumber.Producer.produce("key", "value54")
   end
 end
